c7785917855079a58dd9a6c46c6b5a4c24025d6b
carlapenalva <carlapenalva123@gmail.com>
2024-11-06 11:31:57 -0300

    education

--- src/components/pages/education/EducationTimeline.jsx
+++ src/components/pages/education/EducationTimeline.jsx
@@ -44,11 +44,11 @@ const EducationTimeline = () => {
           padding: "20px",
         }}
       >
-        <Timeline position="alternate">
+        <Timeline position="rigth" sx={{ display: { sm: "none" } }}>
           <TimelineItem>
             <TimelineOppositeContent>
               <Typography variant="body2" sx={{ color: "#FFB0C9" }}>
-                2020 - 2021
+                2022
               </Typography>
             </TimelineOppositeContent>
             <TimelineSeparator>
@@ -63,8 +63,32 @@ const EducationTimeline = () => {
                   padding: "16px",
                 }}
               >
-                <Typography variant="h6">Autónomo / Freelancer</Typography>
-                <Typography>1 Año - Desarrollador web Frontend</Typography>
+                <Typography variant="h6"> Coderhouse </Typography>
+                <Typography>Desarrollador Web</Typography>
+                <Typography variant="body2">HTML, CSS, SASS</Typography>
+              </Box>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent>
+              <Typography variant="body2" sx={{ color: "#FFB0C9" }}>
+                2022
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot sx={{ color: "#FFB0C9" }} />
+              <TimelineConnector sx={{ backgroundColor: "#FFB0C9" }} />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Box
+                sx={{
+                  backgroundColor: "rgb(17, 11, 11)",
+                  borderRadius: "8px",
+                  padding: "16px",
+                }}
+              >
+                <Typography variant="h6"> Coderhouse </Typography>
+                <Typography> JavaScript</Typography>
                 <Typography variant="body2">
                   Desarrollo de Plantillas web estáticas con tecnologías como
                   HTML, CSS, JavaScript, ReactJs, Next.js, Bootstrap y Material
@@ -73,11 +97,10 @@ const EducationTimeline = () => {
               </Box>
             </TimelineContent>
           </TimelineItem>
-
           <TimelineItem>
             <TimelineOppositeContent>
               <Typography variant="body2" sx={{ color: "#FFB0C9" }}>
-                2021 - 2022
+                2023
               </Typography>
             </TimelineOppositeContent>
             <TimelineSeparator>
@@ -92,10 +115,148 @@ const EducationTimeline = () => {
                   padding: "16px",
                 }}
               >
-                <Typography variant="h6">
-                  Autónomo / Freelancer / Pymes
+                <Typography variant="h6"> Coderhouse </Typography>
+                <Typography> React JS</Typography>
+                <Typography variant="body2">
+                  Desarrollo de Plantillas web estáticas con tecnologías como
+                  HTML, CSS, JavaScript, ReactJs, Next.js, Bootstrap y Material
+                  UI.
                 </Typography>
-                <Typography>9 meses - Desarrollador web Fullstack</Typography>
+              </Box>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent>
+              <Typography variant="body2" sx={{ color: "#FFB0C9" }}>
+                2024
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot sx={{ color: "#FFB0C9" }} />
+              <TimelineConnector sx={{ backgroundColor: "#FFB0C9" }} />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Box
+                sx={{
+                  backgroundColor: "rgb(17, 11, 11)",
+                  borderRadius: "8px",
+                  padding: "16px",
+                }}
+              >
+                <Typography variant="h6">Educacion IT</Typography>
+                <Typography> JavaScript Avanzado</Typography>
+                <Typography variant="body2">
+                  Desarrollo y mantenimiento de sitios web Fullstack para
+                  emprendimientos personales de clientes y empresas.
+                </Typography>
+              </Box>
+            </TimelineContent>
+          </TimelineItem>
+        </Timeline>
+        <Timeline
+          position="alternate"
+          sx={{ display: { xs: "none", sm: "block" } }}
+        >
+          <TimelineItem>
+            <TimelineOppositeContent>
+              <Typography variant="body2" sx={{ color: "#FFB0C9" }}>
+                2022
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot sx={{ color: "#FFB0C9" }} />
+              <TimelineConnector sx={{ backgroundColor: "#FFB0C9" }} />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Box
+                sx={{
+                  backgroundColor: "rgb(17, 11, 11)",
+                  borderRadius: "8px",
+                  padding: "16px",
+                }}
+              >
+                <Typography variant="h6"> Coderhouse </Typography>
+                <Typography>Desarrollador Web</Typography>
+                <Typography variant="body2">HTML, CSS, SASS</Typography>
+              </Box>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent>
+              <Typography variant="body2" sx={{ color: "#FFB0C9" }}>
+                2022
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot sx={{ color: "#FFB0C9" }} />
+              <TimelineConnector sx={{ backgroundColor: "#FFB0C9" }} />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Box
+                sx={{
+                  backgroundColor: "rgb(17, 11, 11)",
+                  borderRadius: "8px",
+                  padding: "16px",
+                }}
+              >
+                <Typography variant="h6"> Coderhouse </Typography>
+                <Typography> JavaScript</Typography>
+                <Typography variant="body2">
+                  Desarrollo de Plantillas web estáticas con tecnologías como
+                  HTML, CSS, JavaScript, ReactJs, Next.js, Bootstrap y Material
+                  UI.
+                </Typography>
+              </Box>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent>
+              <Typography variant="body2" sx={{ color: "#FFB0C9" }}>
+                2023
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot sx={{ color: "#FFB0C9" }} />
+              <TimelineConnector sx={{ backgroundColor: "#FFB0C9" }} />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Box
+                sx={{
+                  backgroundColor: "rgb(17, 11, 11)",
+                  borderRadius: "8px",
+                  padding: "16px",
+                }}
+              >
+                <Typography variant="h6"> Coderhouse </Typography>
+                <Typography> React JS</Typography>
+                <Typography variant="body2">
+                  Desarrollo de Plantillas web estáticas con tecnologías como
+                  HTML, CSS, JavaScript, ReactJs, Next.js, Bootstrap y Material
+                  UI.
+                </Typography>
+              </Box>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent>
+              <Typography variant="body2" sx={{ color: "#FFB0C9" }}>
+                2024
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot sx={{ color: "#FFB0C9" }} />
+              <TimelineConnector sx={{ backgroundColor: "#FFB0C9" }} />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Box
+                sx={{
+                  backgroundColor: "rgb(17, 11, 11)",
+                  borderRadius: "8px",
+                  padding: "16px",
+                }}
+              >
+                <Typography variant="h6">Educacion IT</Typography>
+                <Typography> JavaScript Avanzado</Typography>
                 <Typography variant="body2">
                   Desarrollo y mantenimiento de sitios web Fullstack para
                   emprendimientos personales de clientes y empresas.
